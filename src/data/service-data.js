1f81c467471359a11fa697aa963de77a712eafec
Mufizul Islam <121678815+mufizul27@users.noreply.github.com>
2024-02-13 15:51:29 +0600

    all Services section added

--- src/data/service-data.js
+++ src/data/service-data.js
@@ -5,46 +5,122 @@ import service_img_2 from "@assets/img/services/services-2.jpg";
 import service_img_3 from "@assets/img/services/services-3.jpg";
 import service_img_4 from "@assets/img/services/services-4.jpg";
 
+
+// import about_img_1 from "@assets/img/about/web-coding.png";
+// import about_img_2 from "@assets/img/about/web-development.png";
+// import about_img_3 from "@assets/img/about/testing.png";
+// import about_img_4 from "@assets/img/about/app-development.png";
+// import about_img_5 from "@assets/img/about/social-media-marketing.png";
+// import about_img_6 from "@assets/img/about/search-engine-optimization.png";
+// import about_img_7 from "@assets/img/about/content-marketing.png";
+// import about_img_8 from "@assets/img/about/article.png";
+// import about_img_9 from "@assets/img/about/email-marketing.png";
+// import about_img_10 from "@assets/img/about/monitor.png";
+// import about_img_11 from "@assets/img/about/ecommerce.png";
+// import about_img_12 from "@assets/img/about/ecommerce (1).png";
+// import about_img_13 from "@assets/img/about/graphic-design.png";
+// import about_img_14 from "@assets/img/about/marketing.png";
+// import about_img_15 from "@assets/img/about/social-media-marketing.png";
+// import about_img_16 from "@assets/img/about/web-domain.png";
+// import about_img_17 from "@assets/img/about/sms.png";
+
+
+ 
+
 const service_data = [
     {
         id: 1, 
         alphabet: "P",
-        title: <>Product <br /> development</>,
+        title: <>Web<br /> Development</>,
         img: service_img_1,
-        description: <>As far as we might be concerned making an extraordinary As the complexity as </>,        
+        description: <>Our web development services are designed to create dynamic, responsive, and user-friendly websites that reflect your brand's ethos and meet your business objectives</>,        
 
     },
     {
         id: 2, 
         alphabet: "V",
-        title: <>Voice Over <br /> Internet Protocol</>,
+        title: <>Software<br />  Development</>,
         img: service_img_2,
-        description: <>As far as we might be concerned making an extraordinary As the complexity as </>,       
+        description: <>We specialize in developing high-quality software solutions that streamline operations, enhance productivity, and drive business growth.</>,       
 
     },
     {
         id: 3, 
         alphabet: "C",
-        title: <>Clouds <br /> Backup Services</>,
+        title: <> Custom Software <br /> Development
+        </>,
         img: service_img_3,
-        description: <>As far as we might be concerned making an extraordinary As the complexity as </>,        
+        description: <>Tailored software solutions that cater specifically to your business's unique requirements, </>,        
 
     },
     {
         id: 4, 
         alphabet: "M",
-        title: <>Mobile and <br /> Remote Networking</>,
+        title: <>App 
+        <br /> Development</>,
         img: service_img_4,
-        description: <>As far as we might be concerned making an extraordinary As the complexity as </>,        
+        description: <>From conceptualization to launch, we develop mobile applications that offer seamless user experiences on both Android and iOS platforms.
+        </>,        
 
     },
     {
         id: 5, 
         alphabet: "D",
-        title: <>Data Backup <br /> and Restoration</>,
+        title: <>Digital 
+        <br /> Marketing</>,
+        img: service_img_1,
+        description: <>Our digital marketing strategies encompass a broad spectrum of techniques to effectively enhance your online presence and connect with your target audience.
+        </>,        
+    },
+    {
+        id: 6, 
+        alphabet: "E",
+        title: <>Search Engine 
+        <br /> Optimization (SEO)</>,
         img: service_img_2,
-        description: <>As far as we might be concerned making an extraordinary As the complexity as </>,        
+        description: <>Improve your website's visibility on search engines through our comprehensive  SEO strategies, driving organic traffic and elevating your online presence.
+        </>,        
+    },
+    {
+        id: 7, 
+        alphabet: "F",
+        title: <>Content 
 
+        <br /> Marketing</>,
+        img: service_img_3,
+        description: <>Engaging, informative content creation and marketing strategies  designed to attract and retain your target audience.
+        </>,        
+    },
+    {
+        id: 8, 
+        alphabet: "G",
+        title: <>Article 
+        <br /> Writing </>,
+        img: service_img_4,
+        description: <>High-quality, SEO-friendly articles crafted by our team of professional writers to boost your content marketing efforts.
+        </>,        
+    },
+    {
+        id: 9, 
+        alphabet: "H",
+        title: <>Domain 
+ 
+        <br />& Hosting </>,
+        img: service_img_1,
+        description: <>Reliable domain registration and hosting services to ensure your website's optimal performance and accessibility.
+
+        </>,        
+    },
+    {
+        id: 10, 
+        alphabet: "I",
+        title: <>Bulk 
+
+        <br /> SMS</>,
+        img: service_img_2,
+        description: <>Reach out to a wide audience instantly with our bulk SMS services, ideal for promotions, alerts, and updates.
+
+        </>,        
     },
 ]
 export default service_data
